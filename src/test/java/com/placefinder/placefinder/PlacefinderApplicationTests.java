aefba080a934fe7fda259fbf55c549397f7ee32d
kraftenty <kraftenty@gmail.com>
2023-11-08 19:43:17 +0900

    first commit

---
+++ src/test/java/com/placefinder/placefinder/PlacefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placefinder.placefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
